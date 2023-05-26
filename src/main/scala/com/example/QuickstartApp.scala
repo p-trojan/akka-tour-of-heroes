e9562fa8296a9ce706a056bdda04857be6402e6c
Paweł Trojan <absolut84@o2.pl>
2023-05-26 12:05:56 +0200

    checkout dev, cleanup

--- src/main/scala/com/example/QuickstartApp.scala
+++ src/main/scala/com/example/QuickstartApp.scala
@@ -4,12 +4,10 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
-import akka.util.Collections
 import com.example.JsonFormats.heroJsonFormat
 import spray.json.DefaultJsonProtocol.listFormat
 import spray.json._
 
-import scala.collection.immutable.{AbstractSeq, LinearSeq}
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
@@ -17,21 +15,6 @@ import scala.util.{Failure, Success}
 //#main-class
 object QuickstartApp {
   //#start-http-server
-  private def startHttpServer(routes: Route)(implicit system: ActorSystem[_]): Unit = {
-    // Akka HTTP still needs a classic ActorSystem to start
-    import system.executionContext
-
-    val futureBinding = Http().newServerAt("0.0.0.0", 8080).bindFlow(routes)
-    futureBinding.onComplete {
-      case Success(binding) =>
-        val address = binding.localAddress
-        system.log.info("Server online at http://{}:{}/", address.getHostString, address.getPort)
-      case Failure(ex) =>
-        system.log.error("Failed to bind HTTP endpoint, terminating system", ex)
-        system.terminate()
-    }
-  }
-
   private def startHeroesHttpServer(routes: Route)(implicit system: ActorSystem[_]): Unit = {
     import system.executionContext
     val heroesBinding = Http().newServerAt("127.0.0.1", 8080).bindFlow(routes)
@@ -57,12 +40,6 @@ object QuickstartApp {
     //#server-bootstrapping
 
     val rootBehavior = Behaviors.setup[Nothing] { context =>
-//            val userRegistryActor = context.spawn(UserRegistry(), "UserRegistryActor")
-//            context.watch(userRegistryActor)
-//            val routes = new UserRoutes(userRegistryActor)(context.system)
-//            startHttpServer(routes.userRoutes)(context.system)
-
-//      routes.createUser(User("asd", 22, "dasl"))
 
       val heroRegistryActor = context.spawn(HeroRegistry(), "HeroRegistryActor")
       context.watch(heroRegistryActor)
